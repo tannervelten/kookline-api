360f2d5ff94b091bd8d73bcb34040dba30bd4cee
tannervelten <tannervelten@gmail.com>
2018-12-16 16:23:50 -0800

    id route

--- server/app.js
+++ server/app.js
@@ -16,6 +16,15 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.get('/:id', async (req, res) => {
+  try {
+    const result = await axios.get(`https://api.surfline.com/v1/forecasts/${req.params.id}`);
+    res.status(200).json(result.data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
