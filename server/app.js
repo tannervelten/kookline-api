5f527d7cbdf254f92b476502dfcfb498bb4e62d5
tannervelten <tannervelten@gmail.com>
2018-12-16 16:17:23 -0800

    init commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await axios.get('https://api.surfline.com/v1/forecasts/4750');
+    res.status(200).json(result.data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
